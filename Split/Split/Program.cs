96a1fabac7e8b93cf230499b30637c96349381eb
RebecaAlbquuerke <rebecaalbuquerke@gmail.com>
2021-06-10 15:12:04 -0300

    split

---
+++ Split/Split/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 frutas separado por espaço ' ' ");
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+    }
+}
