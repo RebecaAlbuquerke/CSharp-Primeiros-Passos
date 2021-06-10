6dad61a752c0ae12f14a86db682123ace45bf7e0
RebecaAlbquuerke <rebecaalbuquerke@gmail.com>
2021-06-10 15:18:06 -0300

    Ex02

---
+++ Ex02/Ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura = 10.0;
+            double comprimento = 30.0;
+            double valorMetroQuadrado = 200.00;
+
+            double area = largura * comprimento;
+            double preco = area * valorMetroQuadrado;
+
+            Console.WriteLine("area: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("preco: " + preco.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
