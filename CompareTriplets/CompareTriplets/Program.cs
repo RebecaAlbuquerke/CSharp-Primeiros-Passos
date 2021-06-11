91a4cf60332f6143e16d2935e3bfa2db446b45ea
RebecaAlbquuerke <rebecaalbuquerke@gmail.com>
2021-06-11 10:55:32 -0300

    CompareTriplets

---
+++ CompareTriplets/CompareTriplets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompareTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3] { 17, 28, 30 };
+            int[] b = new int[3] { 99, 16, 8 };
+
+            int contadoralice = 0;
+            int contadorbob = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    contadoralice++;
+                }
+                if (b[i] > a[i])
+                {
+                    contadorbob++;
+                }
+            }
+
+            int[] vet = new int[2] { contadoralice, contadorbob };
+
+            foreach (var pontuacao in vet)
+            {
+                Console.WriteLine(pontuacao);
+            }
+        }
+    }
+}
