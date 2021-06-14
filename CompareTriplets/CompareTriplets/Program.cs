b50e0874e4664fe4f07356f5788f8cac62b2939b
RebecaAlbquuerke <rebecaalbuquerke@gmail.com>
2021-06-14 16:01:12 -0300

    Diagonal Difference e Compare Triplets

--- CompareTriplets/CompareTriplets/Program.cs
+++ CompareTriplets/CompareTriplets/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CompareTriplets
 {
@@ -13,6 +14,7 @@ namespace CompareTriplets
             int contadoralice = 0;
             int contadorbob = 0;
 
+            //Usando For
             for (int i = 0; i < 3; i++)
             {
                 if (a[i] > b[i])
@@ -25,12 +27,45 @@ namespace CompareTriplets
                 }
             }
 
+            // Usando linq
+            int contadora = 0;
+            int contadorb = 0;
+            var ab = a.Zip(b, (alice, bob) => (alice: alice, bob: bob));
+            foreach (var rodada in ab)
+            {
+                if (rodada.alice > rodada.bob)
+                {
+                    contadora++;
+                }
+                else if (rodada.bob > rodada.alice)
+                {
+                    contadorb++;
+                }
+            }
+
             int[] vet = new int[2] { contadoralice, contadorbob };
 
             foreach (var pontuacao in vet)
             {
                 Console.WriteLine(pontuacao);
             }
+
+
+            var alicebob = a.Zip(b, (alice, bob) => (alice: alice, bob: bob)).Aggregate((alice: 0, bob: 0), (contador, rodada) =>
+            {
+                if (rodada.alice > rodada.bob)
+                {
+                    return (contador.alice + 1, contador.bob);
+                }
+                else if (rodada.bob > rodada.alice)
+                {
+                    return (contador.alice, contador.bob + 1);
+                }
+                else
+                {
+                    return contador;
+                }
+            });
         }
     }
 }
---
+++ DiagonalDifference/DiagonalDifference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 9, 8, 9 } };           
+
+            int i = 0;
+            int j = 0;
+            int sum1 = 0;
+            while (i < 3 && j < 3)
+            {
+                //Console.WriteLine(arr[i, j]);
+                sum1 += arr[i, j];
+                i++;
+                j++;
+            }
+
+            i = 0;
+            j = 2;
+            int sum2 = 0;
+            while (i < 3 && j >= 0)
+            {
+                //Console.WriteLine(arr[ii, jj]);
+                sum2 += arr[i, j];
+                i++;
+                j--;
+            }
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine ("resultado: " + Math.Abs(sum2 - sum1));
+
+            List<int> lista = new List<int>();
+            List<List<int>> arr2 = new List<List<int>>();
+            var d = arr2[0][0];
+        }
+    }
+}
